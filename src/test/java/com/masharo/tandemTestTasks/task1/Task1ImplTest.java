82af18b93e33232d3b1f04110239c869d84c5911
Masharo <naronwaz0@gmail.com>
2022-07-11 11:44:56 +0500

    Добавил тесты если в столбце все значения null или пустые, если список состоит из одного элемента и если в списке отсутствуют элементы

--- src/test/java/com/masharo/tandemTestTasks/task1/Task1ImplTest.java
+++ src/test/java/com/masharo/tandemTestTasks/task1/Task1ImplTest.java
@@ -8,11 +8,11 @@ import java.util.List;
 
 class Task1ImplTest {
 
-    String[] row0 = {"Hallo fel poll", "8943", "fsdg iufdg u34 gr", "hallo fel poll", ""};
-    String[] row1 = {"fkd jfeljg oi", "9000", "jgf n;trnjrt ", "fkd jfeljg oi", null};
-    String[] row2 = {"Hallo fel poll", "100", "fsdg iufdg u34 gr", "hallo fel poll", "hallo fel poll"};
-    String[] row3 = {"kph kty", "0008", "jm hort5p4g 8", "kph kty", "fkd jfeljg oi"};
-    String[] row4 = {"Kph kty", "0008", "jm hort5p4g 8", "kpa kty", "123"};
+    String[] row0 = {"Hallo fel poll", "8943", "fsdg iufdg u34 gr", "hallo fel poll", "", null, ""};
+    String[] row1 = {"fkd jfeljg oi", "9000", "jgf n;trnjrt ", "fkd jfeljg oi", null, null, ""};
+    String[] row2 = {"Hallo fel poll", "100", "fsdg iufdg u34 gr", "hallo fel poll", "hallo fel poll", null, ""};
+    String[] row3 = {"kph kty", "0008", "jm hort5p4g 8", "kph kty", "fkd jfeljg oi", null, ""};
+    String[] row4 = {"Kph kty", "0008", "jm hort5p4g 8", "kpa kty", "123", null, ""};
 
     @Test
     void sortOnlyInteger() {
@@ -124,5 +124,91 @@ class Task1ImplTest {
         Assertions.assertArrayEquals(expected.get(2), actual.get(2));
         Assertions.assertArrayEquals(expected.get(3), actual.get(3));
         Assertions.assertArrayEquals(expected.get(4), actual.get(4));
+    }
+
+    @Test
+    void sortOnlyNull() {
+        List<String[]> expected = new ArrayList<>() {{
+            add(row0);
+            add(row1);
+            add(row2);
+            add(row3);
+            add(row4);
+        }};
+
+        List<String[]> actual = new ArrayList<>() {{
+            add(row0);
+            add(row1);
+            add(row2);
+            add(row3);
+            add(row4);
+        }};
+
+        IStringRowsListSorter task1 = Task1Impl.getInstance();
+        task1.sort(expected, 5);
+
+        Assertions.assertArrayEquals(expected.get(0), actual.get(0));
+        Assertions.assertArrayEquals(expected.get(1), actual.get(1));
+        Assertions.assertArrayEquals(expected.get(2), actual.get(2));
+        Assertions.assertArrayEquals(expected.get(3), actual.get(3));
+        Assertions.assertArrayEquals(expected.get(4), actual.get(4));
+    }
+
+    @Test
+    void sortOnlyEmpty() {
+        List<String[]> expected = new ArrayList<>() {{
+            add(row0);
+            add(row1);
+            add(row2);
+            add(row3);
+            add(row4);
+        }};
+
+        List<String[]> actual = new ArrayList<>() {{
+            add(row0);
+            add(row1);
+            add(row2);
+            add(row3);
+            add(row4);
+        }};
+
+        IStringRowsListSorter task1 = Task1Impl.getInstance();
+        task1.sort(expected, 6);
+
+        Assertions.assertArrayEquals(expected.get(0), actual.get(0));
+        Assertions.assertArrayEquals(expected.get(1), actual.get(1));
+        Assertions.assertArrayEquals(expected.get(2), actual.get(2));
+        Assertions.assertArrayEquals(expected.get(3), actual.get(3));
+        Assertions.assertArrayEquals(expected.get(4), actual.get(4));
+    }
+
+    @Test
+    void sortOneItem() {
+        List<String[]> expected = new ArrayList<>() {{
+            add(row0);
+        }};
+
+        List<String[]> actual = new ArrayList<>() {{
+            add(row0);
+        }};
+
+        IStringRowsListSorter task1 = Task1Impl.getInstance();
+        task1.sort(expected, 1);
+
+        Assertions.assertArrayEquals(expected.get(0), actual.get(0));
+    }
+
+    @Test
+    void sortZeroItem() {
+        List<String[]> expected = new ArrayList<>();
+
+        try {
+            IStringRowsListSorter task1 = Task1Impl.getInstance();
+            task1.sort(expected, 1);
+            Assertions.assertTrue(expected.isEmpty());
+        } catch (Exception ignored) {
+            Assertions.fail();
+        }
+
     }
 }
